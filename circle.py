bd5397aff03871b822855330d7aa4df11574422f
Jeff Compas <jeffozozo@gmail.com>
2025-09-02 20:41:37 -0600

    Update circle.py
    go back...

--- circle.py
+++ circle.py
@@ -3,18 +3,18 @@ import math
 class Circle:
 
     def __init__(self, radius):
-        def __init__(self, radius):
-    if radius < 0:
-        raise ValueError("Radius must be non-negative")
-    self.mRadius = radius
+        self.mRadius = radius
+        return
 
     def getRadius(self):
         return self.mRadius
 
     def setRadius(self, radius):
-        if radius < 0:
-            raise ValueError("Radius must be non-negative")
-        self.mRadius = radius
+        if radius >= 0.0:
+            self.mRadius = radius
+            return True
+        else:
+            return False
 
     def getArea(self):
         if self.mRadius == 2:
